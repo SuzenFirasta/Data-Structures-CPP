672cad742946b0f0496efa951783c65d63fcbbe6
Suzen Malik Firasta <107317826+SuzenFirasta@users.noreply.github.com>
2024-07-31 22:44:35 +0530

    Update 1_Linear_Search.cpp

--- Searching Alogrithms/1_Linear_Search.cpp
+++ Searching Alogrithms/1_Linear_Search.cpp
@@ -1,1 +1,50 @@
 
+#include <iostream>
+
+using namespace std;
+
+int LinearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int size;
+    cout << "Enter size of your array: ";
+    cin >> size;
+    cout << endl;
+    
+    int arr[size];
+    
+    // Entering elements inside the array
+    for (int i = 0; i < size; i++) {
+        cout << "Enter an element inside your array: ";
+        cin >> arr[i];
+    }
+    
+    cout << endl;
+    
+    cout << "Your array is: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    cout << endl;
+    
+    int target;
+    cout << "Enter a number to find its position: ";
+    cin >> target;
+    
+    int position = LinearSearch(arr, size, target);
+    if (position != -1) {
+        cout << "Element found at index: " << position << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+    
+    return 0;
+}
