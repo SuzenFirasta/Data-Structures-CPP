6a2047ca1ba323ef95f6d4195072179e5f54f3aa
Suzen Malik Firasta <107317826+SuzenFirasta@users.noreply.github.com>
2024-07-31 23:16:36 +0530

    Add files via upload

---
+++ Soritng Alogrithms/4_Bubble_sort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+void BubbleSort(int arr[], int size) 
+{
+    for (int i = 0; i < size - 1; i++) 
+	{
+        for (int j = 0; j < size - 1 - i; j++) 
+		{
+            if (arr[j] > arr[j + 1]) 
+			{
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter size of your array: ";
+    cin >> size;
+    cout << endl;
+
+    int arr[size];
+
+    // Entering elements inside the array
+    for (int i = 0; i < size; i++) 
+	{
+        cout << "Enter an element inside your array: ";
+        cin >> arr[i];
+    }
+
+    cout << endl;
+
+    cout << "Your array is: ";
+    for (int i = 0; i < size; i++) 
+	{
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    BubbleSort(arr, size);
+
+    cout << "Your array after bubble sort is: ";
+    for (int i = 0; i < size; i++) 
+	{
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
