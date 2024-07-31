7bbe6d28ed9cab796d3f24de8d6b1d50d9694e33
Suzen Malik Firasta <107317826+SuzenFirasta@users.noreply.github.com>
2024-07-31 23:12:31 +0530

    Update 1_Selection_Sort.cpp

--- Soritng Alogrithms/1_Selection_Sort.cpp
+++ Soritng Alogrithms/1_Selection_Sort.cpp
@@ -2,11 +2,15 @@
 
 using namespace std;
 
-void SelectionSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
+void SelectionSort(int arr[], int size) 
+{
+    for (int i = 0; i < size - 1; i++) 
+    {
         int minIndex = i;
-        for (int j = i + 1; j < size; j++) {
-            if (arr[j] < arr[minIndex]) {
+        for (int j = i + 1; j < size; j++) 
+        {
+            if (arr[j] < arr[minIndex]) 
+            {
                 minIndex = j;
             }
         }
@@ -26,7 +30,8 @@ int main() {
     int arr[size];
 
     // Entering elements inside the array
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         cout << "Enter an element inside your array: ";
         cin >> arr[i];
     }
@@ -34,7 +39,8 @@ int main() {
     cout << endl;
 
     cout << "Your array is: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         cout << arr[i] << " ";
     }
 
@@ -43,7 +49,8 @@ int main() {
     SelectionSort(arr, size);
     
     cout << "Your array after selection sort is: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         cout << arr[i] << " ";
     }
 
