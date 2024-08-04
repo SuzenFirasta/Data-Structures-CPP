a48f54708e6d1092b5fd0f2f7ec7a2f8c8732067
Suzen Malik Firasta <107317826+SuzenFirasta@users.noreply.github.com>
2024-08-04 21:07:46 +0530

    Update 1_create_2D_Array.cpp

--- 2D_Array/1_create_2D_Array.cpp
+++ 2D_Array/1_create_2D_Array.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
 
+using namespace std;
+
+int main()
+{
+	int rows;
+	cout<<"Enter Number of rows :- ";
+	cin>>rows;
+		
+	int cols;
+	cout<<"Enter Number of columns :- ";
+	cin>>cols;
+	
+	cout<<endl;
+	
+	int a[rows][cols];
+	
+	for(int i=0; i<rows; i++)
+	{
+		for(int j=0; j<cols; j++)
+		{
+			cout<<"Enter an element at "<<i<<"th row and "<<j<<"th column :- ";
+			cin>>a[i][j];
+		}
+	}
+	cout<<endl;
+	cout<<"Your 2D array is :- "<<endl;
+	
+	for(int i=0; i<rows; i++)
+	{
+		for(int j=0; j<cols; j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
